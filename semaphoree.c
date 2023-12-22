4cad681da9751abb7c7b4658717c202714276bde
zeyad ahmed fathallah <93102680+Ziadahmed40@users.noreply.github.com>
2023-12-22 22:06:09 +0000

    adjustments

--- semaphoree.c
+++ semaphoree.c
@@ -14,22 +14,15 @@ struct Queue* createQueue(unsigned capacity)
             = (struct Queue*)malloc(sizeof(struct Queue));
     queue->capacity = capacity;
     queue->front = queue->size = 0;
-
-    // This is important, see the enqueue
     queue->rear = capacity - 1;
     queue->array
             = (int*)malloc(queue->capacity * sizeof(int));
     return queue;
 }
-
-// Queue is full when size becomes
-// equal to the capacity
 int isFull(struct Queue* queue)
 {
     return (queue->size == queue->capacity);
 }
-
-// Queue is empty when size is 0
 int isEmpty(struct Queue* queue)
 {
     return (queue->size == 0);
@@ -43,9 +36,6 @@ void enqueue(struct Queue* queue, int item)
     queue->size = queue->size + 1;
 //    printf("%d enqueued to queue\n", item);
 }
-
-// Function to remove an item from queue.
-// It changes front and size
 int dequeue(struct Queue* queue)
 {
     if (isEmpty(queue))
@@ -54,22 +44,6 @@ int dequeue(struct Queue* queue)
     queue->front = (queue->front + 1) % queue->capacity;
     queue->size = queue->size - 1;
     return item;
-}
-
-// Function to get front of queue
-int front(struct Queue* queue)
-{
-    if (isEmpty(queue))
-        return INT_MIN;
-    return queue->array[queue->front];
-}
-
-// Function to get rear of queue
-int rear(struct Queue* queue)
-{
-    if (isEmpty(queue))
-        return INT_MIN;
-    return queue->array[queue->rear];
 }
 struct Queue *queue;
 #define N 5
